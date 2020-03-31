bdad8e467c87b193ceba49d4dfe4994b5c0ea11b
msikiric <markosikiric@gmail.com>
2020-03-31 18:27:30 +0200

    Add sudo module and other improvements

--- manifests/agent/phpfpm.pp
+++ manifests/agent/phpfpm.pp
@@ -6,11 +6,50 @@
 class zabbix::agent::phpfpm (
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
   $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
-  $php_fpm_sock            = 'undef'
+  $php_fpm_sock            = undef,
 ) inherits zabbix::agent {
 
-  if $php_fpm_sock == undef {
-    fail('Variable $php_fpm_sock must be defined (php-fpm listening socket)')
+  case $::osfamily {
+    default: {
+      $cgi-fcgi = 'fcgi'
+      if $php_fpm_sock == undef {
+        $php_fpm_sock = '127.0.0.1:9000'
+      }
+    }
+    /(RedHat|redhat|amazon)/: {
+      $cgi-fcgi = 'fcgi'
+      if $php_fpm_sock == undef {
+        if $::facts['os']['release']['major'] == '8' {
+          $php_fpm_sock = /run/php-fpm/www.sock
+        }
+        else {
+          $php_fpm_sock = '127.0.0.1:9000'
+        }
+      }
+    }
+    /(Debian|debian|Ubuntu|ubuntu)/: {
+      $cgi-fcgi = 'libcgi-bin'
+      if $php_fpm_sock == undef {
+        if $::facts['os']['release']['major'] == '10' {
+          $php_fpm_sock = /run/php/php7.3-fpm.sock
+        }
+        else {
+          $php_fpm_sock = /run/php/php7.0-fpm.sock
+        }
+      }
+    }
+  }
+
+  package { 'cgi-fcgi' :
+    ensure =>  present,
+    name   => $cgi-fcgi,
+  }
+
+  ::sudoers::allowed_command { 'zabbix_php-fpm':
+    command          => "${dir_zabbix_agent_libdir}/php-fpm.sh",
+    user             => 'zabbix',
+    require_password => false,
+    comment          => 'Zabbix sensor for monitoring php-fpm.',
   }
 
   file { "${dir_zabbix_agentd_confd}/php-fpm.conf" :
@@ -18,7 +57,7 @@ class zabbix::agent::phpfpm (
     owner   => root,
     group   => root,
     mode    => '0644',
-    source  => 'puppet:///modules/zabbix/agent/php-fpm/php-fpm.conf',
+    content => template('zabbix/agent/php-fpm.conf.erb'),
     require => [
       Package['zabbix-agent'],
       File["${dir_zabbix_agent_libdir}/php-fpm.sh"],
@@ -33,14 +72,6 @@ class zabbix::agent::phpfpm (
     group   => root,
     mode    => '0755',
     content => template('zabbix/agent/php-fpm.sh.erb'),
-  }
-
-  file { '/etc/sudoers.d/zabbix_php-fpm' :
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0440',
-    source => 'puppet:///modules/zabbix/agent/php-fpm/zabbix_php-fpm',
   }
 
 }
