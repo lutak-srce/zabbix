df687d4e2c871b4997ca5ae894d920f7c3a5fe31
msikiric <msikiric@srce.hr>
2020-08-14 15:51:08 +0200

    Error fixing

--- manifests/agent/apache.pp
+++ manifests/agent/apache.pp
@@ -4,6 +4,8 @@
 # This module installs zabbix apache plugin
 #
 class zabbix::agent::apache (
+  $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
 ) inherits zabbix::agent {
 
 
@@ -26,19 +28,19 @@ class zabbix::agent::apache (
     content => template('zabbix/agent/apache2.conf.erb'),
     require => [
       File["${dir_zabbix_agent_libdir}/apache2.pl"],
-			Package['zabbix-agent'],
+      Package['zabbix-agent'],
     ],
     notify  => Service['zabbix-agent'],
   }
 
   file { "${dir_zabbix_agent_libdir}/apache2.pl" :
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0755',
-    source => 'puppet:///modules/zabbix/agent/apache2.pl',
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    source  => 'puppet:///modules/zabbix/agent/apache2.pl',
 		require => [
-		  Package["curl"],
+      Package['curl'],
     ],
     notify  => Service['zabbix-agent'],
   }
