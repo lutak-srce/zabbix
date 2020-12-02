b3fbb5ef4baf8c30a56b85fe6c7729a2e6eeb76b
msikiric <markosikiric@gmail.com>
2020-12-02 19:34:03 +0100

    Add apache server-status sensor (#54)
    * Add apache server-status sensor
    
    * Fix indentation
    
    * Add package dependecy to apache sensor
    
    * Error fixing
    
    * Clear tabspaces
    
    * Fix indentation
    
    * Change xargs with tr in php-fpm module
    
    * Add zabbix to apache group in php-fpm module
    
    * Fix apache server-status module
    
    * Add php module for php opcache stats
    
    * Fix indent
    
    * Add opcache stats file
    
    * Tidy opcache module
    
    * Tidy opcache module
    
    * Add opcache stats apache conf
    
    * Fix indentation
    
    * Fix indent
    
    * Remove virtual user from agent.pp
    
    Co-authored-by: msikiric <msikiric@srce.hr>

---
+++ files/agent/opcache/zabbix-opcache.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use LWP::UserAgent;
+use strict;
+
+my $host = shift or die "Usage: $0 host mem/hit";
+my $item = shift or die "Usage: $0 host mem/hit";
+my $timeout = 10;
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+my $ua = LWP::UserAgent->new(timeout=>$timeout);
+my $res = $ua->get("https://$host/opcache-stats");
+if (!$res->is_success) {
+  die("Retrieving data failed: ".$res->status_line);
+}
+
+my @values = split('\n', $res->content);
+if("$item" eq "mem"){
+  my @moj = split(' ', $values[1]);
+  my $out = $moj[1]/(1000 * 1000);
+  print("$out");
+}elsif("$item" eq "hit"){
+  my @moj = split(' ', $values[0]);
+  print("$moj[1]");
+}
---
+++ manifests/agent/opcache.pp
@@ -0,0 +1,59 @@
+#
+# = Class: zabbix::agent::opcache
+#
+# This module installs Zabbix PHP opcache sensor
+#
+class zabbix::agent::opcache (
+  $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
+  $host                    = 'localhost',
+) inherits zabbix::agent {
+
+  include apache
+
+  file { "${dir_zabbix_agentd_confd}/opcache.conf" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('zabbix/agent/opcache.conf.erb'),
+    require => [
+      Package['zabbix-agent'],
+      File["${dir_zabbix_agent_libdir}/zabbix-opcache.pl"],
+    ],
+    notify  => Service['zabbix-agent'],
+  }
+
+  file { "${dir_zabbix_agent_libdir}/zabbix-opcache.pl" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    source  => 'puppet:///modules/zabbix/agent/opcache/zabbix-opcache.pl',
+    require =>  [
+      Package['zabbix-agent'],
+    ],
+  }
+
+  file { "${apache::confd_dir}/opcache_stats.conf" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('zabbix/agent/opcache_stats.conf.erb'),
+    require => [
+      Package['zabbix-agent'],
+      File["${dir_zabbix_agent_libdir}/zabbix-opcache.pl"],
+    ],
+    notify  => Service['zabbix-agent'],
+  }
+
+  file { "${dir_zabbix_agent_libdir}/opcache_stats.php" :
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/zabbix/agent/opcache/opcache_stats.php',
+    notify => Class['apache::service'],
+  }
+}
