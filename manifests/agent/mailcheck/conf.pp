36e7fe2aa7cc68ccfe63c93504e2bd43dc290bf2
Ante Jurjevic <ajurjevi@srce.hr>
2022-04-28 09:17:40 +0200

    Prettfy mailcheck::conf

--- manifests/agent/mailcheck/conf.pp
+++ manifests/agent/mailcheck/conf.pp
@@ -3,13 +3,13 @@
 #
 define zabbix::agent::mailcheck::conf (
   $confname                = $name,
-  $options                 = '',
-  $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
   $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
+  $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
   $imap_host               = '',
-  $smtp_host               = '',
-  $mail_username           = '',
   $mail_password           = '',
+  $mail_username           = '',
+  $options                 = '',
+  $smtp_host               = '',
 ) {
 
   file { "${dir_zabbix_agentd_confd}/mailcheck-${confname}.conf":
