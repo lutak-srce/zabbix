aa9db227a3606dfc725e06f8b68b7454d341acfa
Ante Jurjevic <ajurjevi@srce.hr>
2022-04-28 10:31:57 +0200

    asdf

--- manifests/agent/mailcheck/conf.pp
+++ manifests/agent/mailcheck/conf.pp
@@ -2,17 +2,16 @@
 # This module manages configuration file for zabbix::agent::mailcheck
 #
 define zabbix::agent::mailcheck::conf (
-  $confname                = $name,
   $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
-  $imap_host               = '',
-  $mail_password           = '',
-  $mail_username           = '',
-  $options                 = '',
-  $smtp_host               = '',
+  $imap_host,
+  $mail_password,
+  $mail_username,
+  $options,
+  $smtp_host,
 ) {
 
-  file { "${dir_zabbix_agentd_confd}/mailcheck-${confname}.conf":
+  file { "${dir_zabbix_agentd_confd}/mailcheck-${name}.conf":
     ensure  => file,
     owner   => root,
     group   => root,
