ea443f172bd78e55223cad29cd71c27acc914160
Ante Jurjevic <ajurjevi@srce.hr>
2023-05-09 15:18:52 +0200

    Add chrony sensor to ntp

--- manifests/agent/ntp.pp
+++ manifests/agent/ntp.pp
@@ -8,23 +8,47 @@ class zabbix::agent::ntp (
 ) inherits zabbix::agent {
 
   case $::osfamily {
-    default: {
+    /(RedHat|redhat)/: {
+
       $ntpq_bin = '/usr/sbin/ntpq'
-    }
-    /(RedHat|redhat|amazon)/: {
-      $ntpq_bin = '/usr/sbin/ntpq'
+
+      case $facts['os']['release']['major'] {
+        /^(8|9)/ : {
+          file { "${dir_zabbix_agentd_confd}/chrony.conf" :
+            ensure  => file,
+            owner   => root,
+            group   => root,
+            content => template('zabbix/agent/chrony.conf.erb'),
+            notify  => Service['zabbix-agent'],
+          }
+        }
+        default: {
+          file { "${dir_zabbix_agentd_confd}/ntp.conf" :
+            ensure  => file,
+            owner   => root,
+            group   => root,
+            content => template('zabbix/agent/ntp.conf.erb'),
+            notify  => Service['zabbix-agent'],
+          }
+        }
+      }
+
     }
     /(Debian|debian|Ubuntu|ubuntu)/: {
-      $ntpq_bin = '/usr/bin/ntpq'
-    }
-  }
 
-  file { "${dir_zabbix_agentd_confd}/ntp.conf" :
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    content => template('zabbix/agent/ntp.conf.erb'),
-    notify  => Service['zabbix-agent'],
+      $ntpq_bin = '/usr/bin/ntpq'
+
+      file { "${dir_zabbix_agentd_confd}/ntp.conf" :
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        content => template('zabbix/agent/ntp.conf.erb'),
+        notify  => Service['zabbix-agent'],
+      }
+    }
+
+    default: { notify{'zabbix::agent::ntp module does not support this OS !!!': } }
+
   }
 
 }
