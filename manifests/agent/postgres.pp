35924fab85bcb498113e517e0e1e6f5abe1592c3
Mario Goljak <mgoljak@srce.hr>
2023-12-04 11:04:01 +0100

    Edit postgresql zabbix sensor

--- manifests/agent/postgres.pp
+++ manifests/agent/postgres.pp
@@ -10,11 +10,36 @@ class zabbix::agent::postgres (
   $repo_url                = 'https://git.zabbix.com/scm/zbx/zabbix.git',
   $repo_branch             = 'release/6.0',
   $templates_path          = 'templates/db/postgresql',
+  $zbx_monitor_user        = 'zbx_monitor',
+  $zbx_monitor_password,
 ) inherits zabbix::agent {
 
   # Ensure git is installed
   package { 'git':
     ensure => installed,
+  }
+
+  # Set a global default path for all exec resources
+  Exec {
+    path => ['/usr/bin'],
+  }
+
+  # PostgreSQL user and grant
+  exec { 'add_${zbx_monitor_user}_user':
+    command => "sudo -u postgres psql -c \"CREATE USER ${zbx_monitor_user} WITH PASSWORD '${zbx_monitor_password}';\"",
+    unless  => "sudo -u postgres psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='${zbx_monitor_user}';\"",
+  }
+
+  exec { 'zbx_grant_monitor':
+    command => "sudo -u postgres psql -c 'GRANT pg_monitor TO ${zbx_monitor_user};'",
+    unless  => "sudo -u postgres psql -tAc \"SELECT 1 FROM pg_roles WHERE rolname='${zbx_monitor_user}' AND rolsuper IS FALSE;\"",
+    require => Exec['add_${zbx_monitor_user}_user'],
+  }
+
+  # Cron job for grant connect
+  file { '/etc/cron.d/zbx_grant_connect':
+    content => "* */12 * * * root /usr/bin/psql -t -c \"SELECT format('GRANT CONNECT ON DATABASE %I TO ${zbx_monitor_user};', datname) FROM pg_database\" | /usr/bin/psql -U postgres\n",
+    require => Exec['add_${zbx_monitor_user}_user'],
   }
 
   # Ensure the directory exists
@@ -29,7 +54,6 @@ class zabbix::agent::postgres (
   # Check if the desired directory is empty
   $is_empty = exec { "check-${zabbix_dir}-empty":
     command     => "test -z \"\$(ls -A ${zabbix_dir})\"",
-    path        => ['/bin', '/usr/bin'],
     onlyif      => "test -d ${zabbix_dir}",
     logoutput   => true,
     refreshonly => true,
@@ -42,7 +66,6 @@ class zabbix::agent::postgres (
     # Clone the repository using git commands
     exec { 'git-clone':
       command => "git clone --depth 1 --branch ${repo_branch} ${repo_url} --single-branch ${tmp_dir}",
-      path    => '/usr/bin',
       creates => "${tmp_dir}/.git",
       onlyif  => "test -z \"$(ls -A ${tmp_dir})\"",
     }
@@ -50,7 +73,6 @@ class zabbix::agent::postgres (
     # Configure sparse checkout
     exec { 'config-sparse-checkout':
       command     => "git -C ${tmp_dir} config core.sparseCheckout true",
-      path        => '/usr/bin',
       onlyif      => "test -d ${tmp_dir}/.git",
       refreshonly => true,
       subscribe   => Exec["git-clone"],
@@ -59,7 +81,6 @@ class zabbix::agent::postgres (
     # Define the sparse-checkout file
     exec { 'define-sparse-checkout':
       command => "echo ${templates_path}/ > ${tmp_dir}/.git/info/sparse-checkout",
-      path    => '/usr/bin',
       creates => "${tmp_dir}/.git/info/sparse-checkout",
       require => Exec['config-sparse-checkout'],
     }
@@ -67,7 +88,6 @@ class zabbix::agent::postgres (
     # Checkout the desired branch and pull changes
     exec { 'git-pull':
       command     => "git -C ${tmp_dir} checkout ${repo_branch}; git -C ${tmp_dir} pull",
-      path        => '/usr/bin',
       require     => Exec['define-sparse-checkout'],
       refreshonly => true,
       subscribe   => Exec["git-clone"],
