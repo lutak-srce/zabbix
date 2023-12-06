8afcd5a226dc3131e5a5e0e4af86745bfdf3dbde
Mario Goljak <mgoljak@srce.hr>
2023-12-06 10:56:58 +0100

    revert last commit

--- manifests/agent/postgresql.pp
+++ manifests/agent/postgresql.pp
@@ -30,13 +30,13 @@ class zabbix::agent::postgresql (
       require => Postgresql::Server::Role[$zbx_monitor_user],
     }
 
-#    file { $dir_zabbix_pg_template:
-#      ensure  => directory,
-#      owner   => 'zabbix',
-#      group   => 'zabbix',
-#      mode    => '0770',
-#      require => Package['zabbix-agent'],
-#    }
+    file { $dir_zabbix_pg_template:
+      ensure  => directory,
+      owner   => 'zabbix',
+      group   => 'zabbix',
+      mode    => '0770',
+      require => Package['zabbix-agent'],
+    }
 
     file { "${dir_zabbix_pg_template}":
       ensure  => directory,
