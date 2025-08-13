50080e9dcce7b0a414f63b388ae0c8ee1e8c625f
Zavisa Nikac <znikac@srce.hr>
2025-08-13 22:57:58 +0200

    Instead of installing libpostfix-zabbix-agent package replicate its config and files

--- manifests/agent/postfix.pp
+++ manifests/agent/postfix.pp
@@ -7,11 +7,33 @@
 #
 class zabbix::agent::postfix (
   $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
 ) inherits zabbix::agent {
 
-  package { 'libpostfix-zabbix-agent':
-    ensure  => present,
+  sudoers::allowed_command { 'zabbix_postfix' :
+    command          => "${dir_zabbix_agent_libdir}/postfix.pl",
+    user             => 'zabbix',
+    run_as           => 'ALL',
+    require_password => false,
+  }
+
+  file { "${dir_zabbix_agentd_confd}/postfix.conf" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    content => template('zabbix/agent/postfix.conf.erb'),
+    notify  => Service['zabbix-agent'],
     require => Package['zabbix-agent'],
+  }
+
+  file { "${dir_zabbix_agent_libdir}/postfix.pl" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    source  => 'puppet:///modules/zabbix/agent/postfix.pl',
+    notify  => Service['zabbix-agent'],
+    require => ::Sudoers::Allowed_command['zabbix_postfix'],
   }
 
 }
