27e98c77eb3798a652797a9ee00f0f9b3e6be504
Zavisa Nikac <znikac@srce.hr>
2025-08-17 23:10:35 +0200

    remove disfunctional notify_service param and redundant attributes, due to agent.pp File declaration override

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -5,21 +5,11 @@
 # directory.
 define zabbix::agent::config (
   $settings,
-  $notify_service = true,
 ) {
-  $service_to_notify = $notify_service ? {
-    default => undef,
-    true    => Service[$zabbix::agent::service_state],
-  }
-
   file { "${zabbix::agent::conf_dir}/${name}.conf":
     ensure  => file,
     content => template('zabbix/custom.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
     require => File[$zabbix::agent::conf_dir],
-    notify  => $service_to_notify,
   }
 
 }
