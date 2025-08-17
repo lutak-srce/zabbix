17bd85be5313c6ec54a6f1d16caf697a58280b68
Zavisa Nikac <znikac@srce.hr>
2025-08-17 09:34:07 +0200

    add NFS server sensor

---
+++ manifests/agent/nfsserver.pp
@@ -0,0 +1,15 @@
+# @summary 
+#   Manages Zabbix agent configuration for NFS server monitoring.
+#
+# @example
+#   include zabbix::agent::nfsserver
+#
+# @note 
+#   This class inherits all parameters from zabbix::agent class.
+#
+class zabbix::agent::nfsserver inherits zabbix::agent {
+  file { "${::zabbix::agent::conf_dir}/nfs_server.conf" :
+    ensure => file,
+    source => 'puppet:///modules/zabbix/agent/nfs_server.conf',
+  }
+}
