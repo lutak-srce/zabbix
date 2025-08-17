b94f6a557db097be3d503970902efaf088ea90fe
Zavisa Nikac <znikac@srce.hr>
2025-08-17 21:46:11 +0200

    fix a regression

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -5,24 +5,20 @@
 # directory.
 define zabbix::agent::config (
   $settings,
-  $conf_dir       = $zabbix::agent::conf_dir,
-  $agent_service  = $zabbix::agent::agent_service,
   $notify_service = true,
 ) {
-  include ::zabbix::agent
-
   $service_to_notify = $notify_service ? {
     default => undef,
-    true    => Service[$agent_service],
+    true    => Service[$zabbix::agent::service_state],
   }
 
-  file { "${conf_dir}/${name}.conf":
+  file { "${zabbix::agent::conf_dir}/${name}.conf":
     ensure  => file,
     content => template('zabbix/custom.conf.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    require => File[$conf_dir],
+    require => File[$zabbix::agent::conf_dir],
     notify  => $service_to_notify,
   }
 
