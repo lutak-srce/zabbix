ca9820283490e7bf5fc0507ae98a6710f184621b
Zavisa Nikac <znikac@srce.hr>
2025-08-18 19:51:05 +0200

    Refactor: Group params by component for clarity

--- manifests/params.pp
+++ manifests/params.pp
@@ -29,6 +29,28 @@ class zabbix::params {
   $agent_status            = 'enabled'
   $file_zabbix_agentd_conf = '/etc/zabbix/zabbix_agentd.conf'
   $zabbix_agent_pidfile    = '/var/run/zabbix/zabbix_agentd.pid'
+  # OS family specific params
+  case $facts['os']['family'] {
+    /(Debian|debian|Ubuntu|ubuntu)/: {
+      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.conf.d'
+      $dir_zabbix_agent_libdir  = '/usr/lib/zabbix-agent'
+      $dir_zabbix_agent_modules = '/usr/lib/zabbix-agent/modules'
+      $zabbix_agentd_logfile    = '/var/log/zabbix-agent/zabbix_agentd.log'
+    }
+    /(RedHat|redhat|amazon)/: {
+      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.d'
+      $dir_zabbix_agent_libdir  = '/usr/libexec/zabbix-agent'
+      $dir_zabbix_agent_modules = '/usr/libexec/zabbix-agent/modules'
+      $zabbix_agentd_logfile    = '/var/log/zabbix/zabbix_agentd.log'
+    }
+    default: {
+      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.d'
+      $dir_zabbix_agent_libdir  = '/usr/lib/zabbix/agent'
+      $dir_zabbix_agent_modules = '/usr/lib/zabbix/agent/modules'
+      $zabbix_agentd_logfile    = '/var/log/zabbix/zabbix_agentd.log'
+    }
+  }
+
 
   # agent2 settings
   $agent2_package             = 'zabbix-agent2'
@@ -52,6 +74,37 @@ class zabbix::params {
   $server_status           = 'enabled'
   $file_zabbix_server_conf = '/etc/zabbix/zabbix_server.conf'
   $dir_zabbix_server_confd = '/etc/zabbix/zabbix_server.d'
+  # OS family specific params
+  case $facts['os']['family'] {
+    /(Debian|debian|Ubuntu|ubuntu)/: {
+      $zabbix_server_logfile = '/var/log/zabbix/zabbix_server.log'
+      $zabbix_server_pidfile = '/var/run/zabbix/zabbix_server.pid'
+      $fpinglocation         = '/usr/bin/fping'
+      $fping6location        = '/usr/bin/fping6'
+      $alert_scripts_path    = '/usr/lib/zabbix/alertscripts'
+      $external_scripts      = '/usr/lib/zabbix/externalscripts'
+      $tmpdir                = '/tmp'
+    }
+    /(RedHat|redhat|amazon)/: {
+      $zabbix_server_logfile = '/var/log/zabbixsrv/zabbix_server.log'
+      $zabbix_server_pidfile = '/var/run/zabbixsrv/zabbix_server.pid'
+      $fpinglocation         = '/usr/sbin/fping'
+      $fping6location        = '/usr/sbin/fping6'
+      $alert_scripts_path    = '/var/lib/zabbixsrv/alertscripts'
+      $external_scripts      = '/var/lib/zabbixsrv/externalscripts'
+      $tmpdir                = '/var/lib/zabbixsrv/tmp'
+    }
+    default: {
+      $zabbix_server_logfile = '/var/log/zabbix/zabbix_server.log'
+      $zabbix_server_pidfile = '/var/run/zabbix/zabbix_server.pid'
+      $fpinglocation         = '/usr/bin/fping'
+      $fping6location        = '/usr/bin/fping6'
+      $alert_scripts_path    = '/var/lib/zabbixsrv/alertscripts'
+      $external_scripts      = '/var/lib/zabbixsrv/externalscripts'
+      $tmpdir                = '/tmp'
+    }
+  }
+
 
   # proxy settings
   $proxy_file_owner       = 'root'
@@ -82,73 +135,21 @@ class zabbix::params {
   $web_file_owner     = 'root'
   $web_file_mode      = '0640'
   $web_dir_zabbix_php = '/etc/zabbix/web'
-
-  # module dependencies
-  $dependency_class = 'zabbix::dependency'
-  $my_class         = undef
-
   # OS family specific params
   case $facts['os']['family'] {
-    /(RedHat|redhat|amazon)/: {
-      # agent settings
-      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.d'
-      $dir_zabbix_agent_libdir  = '/usr/libexec/zabbix-agent'
-      $dir_zabbix_agent_modules = '/usr/libexec/zabbix-agent/modules'
-      $zabbix_agentd_logfile    = '/var/log/zabbix/zabbix_agentd.log'
-
-      # server settings
-      $zabbix_server_logfile = '/var/log/zabbixsrv/zabbix_server.log'
-      $zabbix_server_pidfile = '/var/run/zabbixsrv/zabbix_server.pid'
-      $fpinglocation         = '/usr/sbin/fping'
-      $fping6location        = '/usr/sbin/fping6'
-      $alert_scripts_path    = '/var/lib/zabbixsrv/alertscripts'
-      $external_scripts      = '/var/lib/zabbixsrv/externalscripts'
-      $tmpdir                = '/var/lib/zabbixsrv/tmp'
-
-      # web settings
-      $web_package    = 'zabbix-web'
-      $web_file_group = 'root'
-    }
     /(Debian|debian|Ubuntu|ubuntu)/: {
-      # agent settings
-      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.conf.d'
-      $dir_zabbix_agent_libdir  = '/usr/lib/zabbix-agent'
-      $dir_zabbix_agent_modules = '/usr/lib/zabbix-agent/modules'
-      $zabbix_agentd_logfile    = '/var/log/zabbix-agent/zabbix_agentd.log'
-
-      # server settings
-      $zabbix_server_logfile = '/var/log/zabbix/zabbix_server.log'
-      $zabbix_server_pidfile = '/var/run/zabbix/zabbix_server.pid'
-      $fpinglocation         = '/usr/bin/fping'
-      $fping6location        = '/usr/bin/fping6'
-      $alert_scripts_path    = '/usr/lib/zabbix/alertscripts'
-      $external_scripts      = '/usr/lib/zabbix/externalscripts'
-      $tmpdir                = '/tmp'
-      
-      # web settings
       $web_package    = 'zabbix-frontend-php'
       $web_file_group = 'www-data'
     }
     default: {
-      # agent settings
-      $dir_zabbix_agentd_confd  = '/etc/zabbix/zabbix_agentd.d'
-      $dir_zabbix_agent_libdir  = '/usr/lib/zabbix/agent'
-      $dir_zabbix_agent_modules = '/usr/lib/zabbix/agent/modules'
-      $zabbix_agentd_logfile    = '/var/log/zabbix/zabbix_agentd.log'
-
-      # server settings
-      $zabbix_server_logfile = '/var/log/zabbix/zabbix_server.log'
-      $zabbix_server_pidfile = '/var/run/zabbix/zabbix_server.pid'
-      $fpinglocation         = '/usr/bin/fping'
-      $fping6location        = '/usr/bin/fping6'
-      $alert_scripts_path    = '/var/lib/zabbixsrv/alertscripts'
-      $external_scripts      = '/var/lib/zabbixsrv/externalscripts'
-      $tmpdir                = '/tmp'
-
-      # web settings
       $web_package    = 'zabbix-web'
       $web_file_group = 'root'
     }
   }
+
+
+  # module dependencies
+  $dependency_class = 'zabbix::dependency'
+  $my_class         = undef
 
 }
