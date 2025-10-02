2d1053b9cc8f5e152348809e975720eb60656e93
Mihael Burlic <mburlic@srce.hr>
2025-10-02 12:31:05 +0200

    Add agent::php-fpm-opcache class

---
+++ manifests/agent/phpfpmopcache.pp
@@ -0,0 +1,31 @@
+#
+# = Class: zabbix::agent::phpfpm
+#
+# This module installs Zabbix php-fpm sensor
+#
+class zabbix::agent::phpfpm (
+  $dir_zabbix_agentd_confd = $::zabbix::agent::dir_zabbix_agentd_confd,
+  $dir_zabbix_agent_libdir = $::zabbix::agent::dir_zabbix_agent_libdir,
+) inherits zabbix::agent {
+
+  file { "${dir_zabbix_agentd_confd}/php-fpm-opcache.conf" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('zabbix/agent/php-fpm-opcache.conf.erb'),
+    require => [
+      Package['zabbix-agent'],
+      File["${dir_zabbix_agent_libdir}/php-fpm-opcache.php"],
+    ],
+    notify  => Service['zabbix-agent'],
+  }
+
+  file { "${dir_zabbix_agent_libdir}/php-fpm-opcache.php" :
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    content => template('zabbix/agent/php-fpm-opcache.php.erb'),
+  }
+}
